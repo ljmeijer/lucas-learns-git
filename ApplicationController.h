6095e3de5a12b3e70b51e86afe5f5e639e8c54f7
unknown <Lucas Meijer@.(none)>
2009-12-05 14:11:42 +0100

    one more feature

--- ApplicationController.h
+++ ApplicationController.h
@@ -5,6 +5,7 @@
 //  Created by Pieter de Bie on 13-06-08.
 //  Copyright __MyCompanyName__ 2008 . All rights reserved.
 //  Lucas also did some stuff
+//  Add another small feature
 
 #import <Cocoa/Cocoa.h>
 #import "PBGitRepository.h"
